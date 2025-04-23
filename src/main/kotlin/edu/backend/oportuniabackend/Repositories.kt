629431af6cae6da4d74bf882c2ace876da0911e6
Alex Marín Quesada <116366625+AlexMarinQ@users.noreply.github.com>
2025-04-23 12:09:33 -0600

    #4 Create Repositories

---
+++ src/main/kotlin/edu/backend/oportuniabackend/Repositories.kt
@@ -0,0 +1,83 @@
+package edu.backend.oportuniabackend.repository
+
+import edu.backend.oportuniabackend.*
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByEmail(@Param("email") email: String): Optional<User>
+}
+
+@Repository
+interface RoleRepository : JpaRepository<Role, Long> {
+    fun findByName(@Param("name") name: String): Optional<Role>
+}
+
+@Repository
+interface PrivilegeRepository : JpaRepository<Privilege, Long>
+
+@Repository
+interface StudentRepository : JpaRepository<Student, Long>
+
+@Repository
+interface AdminRepository : JpaRepository<Admin, Long>
+
+@Repository
+interface AbilityRepository : JpaRepository<Ability, Long>{
+    fun findByStudentId(@Param("studentId") studentId: Long): List<Ability>
+}
+
+@Repository
+interface AdviceRepository : JpaRepository<Advice, Long>
+
+@Repository
+interface CompanyRepository : JpaRepository<Company, Long>
+
+@Repository
+interface CompanyReviewRepository : JpaRepository<CompanyReview, Long>{
+    fun findByCompanyId(@Param("companyId") companyId: Long): List<CompanyReview>
+}
+
+@Repository
+interface CurriculumRepository : JpaRepository<Curriculum, Long>{
+    fun findByStudentId(@Param("studentId") studentId: Long): List<Curriculum>
+}
+
+@Repository
+interface EducationRepository : JpaRepository<Education, Long>{
+    fun findByStudentId(@Param("studentId") studentId: Long): List<Education>
+}
+
+@Repository
+interface ExperienceRepository : JpaRepository<Experience, Long>{
+    fun findByStudentId(@Param("studentId") studentId: Long): List<Experience>
+}
+
+@Repository
+interface InterviewRepository : JpaRepository<Interview, Long>{
+    fun findByStudentId(@Param("studentId") studentId: Long): List<Interview>
+}
+
+@Repository
+interface IAAnalysisRepository : JpaRepository<IAAnalysis, Long>{
+    fun findByInterviewId(interviewId: Long): IAAnalysis?
+    fun findByCurriculumId(curriculumId: Long): IAAnalysis?
+}
+
+@Repository
+interface NotificationRepository : JpaRepository<Notification, Long>{
+    fun findByUserId(@Param("userId") userId: Long): List<Notification>
+}
+
+@Repository
+interface StreakRepository : JpaRepository<Streak, Long>{
+    fun findByStudentId(@Param("studentId") studentId: Long): Streak?
+}
+
+@Repository
+interface StudentProgressRepository : JpaRepository<StudentProgress, Long>{
+    fun findByStudentId(@Param("studentId") studentId: Long): StudentProgress?
+}
