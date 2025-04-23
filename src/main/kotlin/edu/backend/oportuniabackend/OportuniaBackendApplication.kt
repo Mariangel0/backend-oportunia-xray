121cb4286504ffaced57a94601b5db23c646a9d1
Ana <ana.avendano.ramirez@est.una.ac.cr>
2025-04-22 20:05:17 -0600

    Initial commit for OportuniaBackend project.

---
+++ src/main/kotlin/edu/backend/oportuniabackend/OportuniaBackendApplication.kt
@@ -0,0 +1,11 @@
+package edu.backend.oportuniabackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OportuniaBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<OportuniaBackendApplication>(*args)
+}
---
+++ src/test/kotlin/edu/backend/oportuniabackend/OportuniaBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.backend.oportuniabackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OportuniaBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
