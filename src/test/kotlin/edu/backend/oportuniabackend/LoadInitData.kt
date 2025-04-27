c03f2fe5f0ca551214b221197eaf31e4bad7653c
Ana <ana.avendano.ramirez@est.una.ac.cr>
2025-04-26 20:42:03 -0600

    Refactor: Update test data deletion and add company and company review tests

--- src/test/kotlin/edu/backend/oportuniabackend/LoadInitData.kt
+++ src/test/kotlin/edu/backend/oportuniabackend/LoadInitData.kt
@@ -13,7 +13,7 @@ import org.springframework.test.context.jdbc.Sql
 @Sql(
     statements = [
         "DELETE FROM public.users",
-        "DELETE FROM public.company"
+        "DELETE FROM public.companies"
     ],
     executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
 )
@@ -23,9 +23,16 @@ import org.springframework.test.context.jdbc.Sql
 )
 class LoadInitData (
     @Autowired
-    val userRepository: UserRepository
+    val userRepository: UserRepository,
+
+    @Autowired
+    val companyRepository: CompanyRepository,
+
+    @Autowired
+    val companyReviewRepository: CompanyReviewRepository,
 )
 {
+    // Users
     @Test
     fun testUserFindAll(){
         val userList: List<User> = userRepository.findAll()
@@ -43,4 +50,38 @@ class LoadInitData (
         val user: User = userRepository.findByEmail("user4@example.com").get()
         Assertions.assertTrue(user.id?.toInt() == 4)
     }
+
+    // Companies
+    @Test
+    fun testCompanyFindAll(){
+        val companyList: List<Company> = companyRepository.findAll()
+        Assertions.assertTrue(companyList.size == 3)
+    }
+
+    @Test
+    fun testCompanyFindById() {
+        val company: Company = companyRepository.findById(1).get()
+        Assertions.assertTrue(company.id?.toInt() == 1)
+    }
+
+    // Companies Reviews
+    @Test
+    fun testCompanyReviewFindAll(){
+        val companyReviewList: List<CompanyReview> = companyReviewRepository.findAll()
+        Assertions.assertTrue(companyReviewList.size == 3)
+    }
+
+    @Test
+    fun testCompanyReviewFindById() {
+        val companyReview: CompanyReview = companyReviewRepository.findById(1).get()
+        Assertions.assertTrue(companyReview.id?.toInt() == 1)
+    }
+
+    @Test
+    fun testCompanyReviewFindByCompanyId() {
+        val companyReviewList: List<CompanyReview> = companyReviewRepository.findByCompanyId(1)
+        Assertions.assertTrue(companyReviewList.size == 1)
+    }
+
+
 }
